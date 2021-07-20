f514cb6a2dfa1ec2b377ab3337d1619a5862472a
Pinny Geliebter <pgibralter@gmail.com>
2021-07-20 10:00:15 -0400

    adds movie index view to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <div>
+      <button v-on:click="indexMovies()">Load Movies</button>
+      <ol>
+        <h2>Movies</h2>
+        <li v-for="movie in movies" v-bind:key="movie.id">{{ movie.title }}</li>
+      </ol>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Hello wosdafrasdfasdfld!",
+      movies: [],
+    };
+  },
+  // created: function () {
+  //   this.indexMovies();
+  // },
+  methods: {
+    indexMovies: function () {
+      axios.get("http://localhost:3000/movies").then((response) => {
+        this.movies = response.data;
+        console.log(response.data);
+      });
+    },
   },
 };
 </script>
